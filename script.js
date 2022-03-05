971bb4d6b9867a613ad462391338c7c951751238
Stefan3002 <92794548+Stefan3002@users.noreply.github.com>
2022-03-05 23:31:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const addBtn = document.querySelector(".AddItemBtn")
 addBtn.addEventListener("click", addItem)
 const events = document.querySelector(".AddedItems")
 
+//Check for saved items in memory
+document.addEventListener("load",getToDos)
+//Check for saved items in memory
+function getToDos(){
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos !== null){
+        for(i = 0; i < todos.length; i++) {
+            //        Create Items
+            const newItem = document.createElement("div")
+            newItem.classList.add("item")
+            newItem.innerHTML = "<i class=\"fa-2x fa-solid delete fa-trash-can\"></i> <i class=\" fa-2x fa-solid completed fa-check\"></i> <h3 class='item-text'>" + todos[i] + "</h3>"
+            newItem.addEventListener("click", handleClick)
+            events.appendChild(newItem)
+        }
+    }
+}
+
 //Keyboard Enter Press
 document.addEventListener("keydown", function(event){
     if(event.code === "Enter")
@@ -25,6 +42,7 @@ function recolor(item){
 
 function addItem(){
     const input = document.querySelector("#Item")
+
     //Validation
     let validationResult = validate(input)
     if(validationResult !== 1) {
@@ -33,6 +51,7 @@ function addItem(){
         input.value = ""
     }
     else {
+        localStorageAdd(input.value)
         const newItem = document.createElement("div")
         newItem.classList.add("item")
         newItem.innerHTML = "<i class=\"fa-2x fa-solid delete fa-trash-can\"></i> <i class=\" fa-2x fa-solid completed fa-check\"></i> <h3 class='item-text'>" + input.value + "</h3>"
@@ -43,11 +62,35 @@ function addItem(){
     }
 }
 function handleClick(event){
-    if(event.target.classList[2] === "delete")
+    if(event.target.classList[2] === "delete") {
         event.target.parentElement.remove()
+    //Remove from local storage as well
+        localStorageRemove(event.target.parentElement)
+    }
     else if(event.target.classList[2] === "completed")
         event.target.parentElement.classList.add("done")
 }
 
+function localStorageAdd(todo){
+    let todos
+    if(localStorage.getItem('todos') === null)
+        todos = []
+    else
+        todos = JSON.parse(localStorage.getItem("todos"))
+    todos.push(todo)
+    localStorage.setItem("todos",JSON.stringify(todos))
+}
+
+function localStorageRemove(item){
+    let todos = JSON.parse(localStorage.getItem("todos"))
+    for(i = 0; i < todos.length ; i++) {
+        console.log(todos[i],item.textContent)
+        if (todos[i].localeCompare(item.textContent)) {
+            todos.splice(i, 1)
+            break
+        }
+    }
+    localStorage.setItem("todos",JSON.stringify(todos))
+}
 
 
